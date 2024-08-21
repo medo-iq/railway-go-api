f6a3dd75a41ca30217d7c6aab374cace78508def
MEDO <36336354+medo-iq@users.noreply.github.com>
2024-08-21 05:10:45 +0300

    Update routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,10 +6,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// دالة معالج للمسار الجذري
+func (app *application) rootHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome to the API!"))
+}
+
+// دالة لتكوين المسارات
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	// Define the available routes
+	// تعريف المسارات المتاحة
+	router.HandlerFunc(http.MethodGet, "/", app.rootHandler) // إضافة المسار الجذري
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	return router
